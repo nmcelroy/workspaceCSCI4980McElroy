6143597f0b55ff20f4b87874b5965e55f94f6d43
Nicole McElroy <nmcelroy@unomaha.edu>
2018-10-09 17:30:28 -0500

    project 1009

---
+++ project1004-type-ast-rename-McElroy/src/util/PathUtil.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.File;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+import project1004_type_ast_rename_mcelroy.Activator;
+
+
+public class PathUtil {
+	public static String getNeighborProjectPath(String projectName) {
+		String thisPluginID = Activator.getContext().getBundle().getSymbolicName();
+		Bundle plugin = Platform.getBundle(thisPluginID);
+		URL url = plugin.getEntry("/");
+		String result = null;
+		try {
+			URL resolvedURL = FileLocator.resolve(url);
+			File file = new File(resolvedURL.getFile());
+			File parentFile = file.getParentFile();
+			File neighborProject = new File(parentFile.getAbsolutePath() + File.separator + projectName);
+			result = neighborProject.getAbsolutePath();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
